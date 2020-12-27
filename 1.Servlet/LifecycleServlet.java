347e7311a3254756e839362d57ef82e7f70edb2f
honggoii <46019755+honggoii@users.noreply.github.com>
2020-12-27 23:00:49 +0900

    Lifecycle of Servlet

---
+++ 1.Servlet/LifecycleServlet.java
@@ -0,0 +1,44 @@
+package examples;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LifecycleServlet")
+public class LifecycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LifecycleServlet() {
+        System.out.println("LifecycleServlet 생성!!");
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init test 호출!!");
+	}
+
+	public void destroy() {
+		System.out.println("destroy 호출!!");
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+//	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("service 호출!!");
+//	}
+
+	
+}
